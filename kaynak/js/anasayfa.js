e1be19b0e050bc02222315ca660c393f4af39f77
Ümit <90252797+umityildirim34@users.noreply.github.com>
2023-10-17 17:32:19 +0300

    Delete kaynak directory

--- kaynak/js/anasayfa.js
+++
@@ -1,9 +0,0 @@
-var tusKarsila = document.getElementById("asf-tus-karsila")
-var etkCikti = document.getElementById("asf-etk-cikti")
-
-
-
-tusKarsila.onclick = function()
-{
-    etkCikti.innerText = "Hoşgeldin."
-}
