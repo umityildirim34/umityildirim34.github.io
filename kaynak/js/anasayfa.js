d4fa34c8fc3ebc282552051d02119dbc8f1a00b8
Ümit <90252797+umityildirim34@users.noreply.github.com>
2023-10-17 14:30:26 +0300

    Add files via upload

---
+++ kaynak/js/anasayfa.js
@@ -0,0 +1,9 @@
+var tusKarsila = document.getElementById("asf-tus-karsila")
+var etkCikti = document.getElementById("asf-etk-cikti")
+
+
+
+tusKarsila.onclick = function()
+{
+    etkCikti.innerText = "Hoşgeldin."
+}
